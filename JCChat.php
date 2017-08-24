fd5b23c853a07e2fc504d77ff0d545e0199d1642
Anton00756 <30309924+Anton00756@users.noreply.github.com>
2017-08-24 17:09:12 +0300

    Add files via upload

---
+++ JCChat.php
@@ -0,0 +1,13 @@
+<?php
+
+$ChatNick = $_GET['ChatNick'];
+$ChatText = $_GET['ChatText'];
+
+$file = 'JCChat.txt';
+
+$current = file_get_contents($file);
+$current .= "{J}$ChatNick: $ChatText{JC}\n";
+
+file_put_contents($file, $current);
+
+?>
